524f1bcf351f934058e01b849721e745c9cffa0c
Michael129-55 <nturamamichael@gmail.com>
2025-05-02 13:04:53 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+//codes to change hero images manually
+var sildeIndex = 1;
+function changeDiv(n){
+    showDiv(sildeIndex += n);
+}
+function showDiv(n){
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if( n > x.length){sildeIndex = 1}
+    if( n < 1){sildeIndex = x.length}
+    for( i = 0; i < x.length; i++){
+        x[i].style.display = 'none';
+    }
+    x[sildeIndex-1].style.display = 'block';
+}
+showDiv(sildeIndex);
+
